7d9a681ec27c3615d02e65f50c4531563cb39080
JADE PARK <dev.jadepark@gmail.com>
2022-11-01 00:59:58 +0900

    volar 변경

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.esm.min.js'
+
+const app = createApp(App)
+app.provide('today', new Date().toISOString().split('T')[0])  //today변수를 provide하고 있으므로 모든 컴포넌트는 today를 inject할 수 있다
+app.mount('#app')
